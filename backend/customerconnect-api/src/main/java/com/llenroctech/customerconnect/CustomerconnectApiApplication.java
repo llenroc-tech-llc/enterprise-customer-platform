7bbc3ce3b535617c44e7e4738e808028add19438
Cornell Reddick <cornellreddick@github.com>
2026-07-04 11:07:46 -0400

    Initialize CustomerConnect Spring Boot API

---
+++ backend/customerconnect-api/src/main/java/com/llenroctech/customerconnect/CustomerconnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.llenroctech.customerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerconnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerconnectApiApplication.class, args);
+	}
+
+}
---
+++ backend/customerconnect-api/src/test/java/com/llenroctech/customerconnect/CustomerconnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llenroctech.customerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerconnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
