4de6668e7ce62ec828aa73355cc2744838cb84b1
Llenroc Tech LLC <cornellreddick@llenroctech.com>
2026-07-06 15:48:37 -0400

    feat: add user DTO, mapper, and service contract

---
+++ backend/customerconnect-api/src/main/java/com/llenroctech/customerconnect/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.llenroctech.customerconnect.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+@Data
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phone;
+    private String title;
+    private String bio;
+    private String imageUrl;
+    private boolean enabled;
+    private boolean isNotLocked = true;
+    private boolean isUsingMfa;
+    private LocalDateTime createdAt;
+}
