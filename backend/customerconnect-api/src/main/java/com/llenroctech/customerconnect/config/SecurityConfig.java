936fcd72b6caa4cec80f0997fb60600950ebea49
Llenroc Tech LLC <cornellreddick@llenroctech.com>
2026-07-09 16:29:32 -0400

    Add EnableMethodSecurity annotation to SecurityConfig

--- backend/customerconnect-api/src/main/java/com/llenroctech/customerconnect/config/SecurityConfig.java
+++ backend/customerconnect-api/src/main/java/com/llenroctech/customerconnect/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final BCryptPasswordEncoder encoder;
