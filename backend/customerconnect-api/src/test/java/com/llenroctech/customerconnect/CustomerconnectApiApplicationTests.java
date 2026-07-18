02d1ed0d7f833e4f3a0dbcc93ff7f213594bcf3b
Llenroc Tech LLC <cornellreddick@llenroctech.com>
2026-07-18 11:50:06 -0400

    fix: add MockitoBean

--- backend/customerconnect-api/src/test/java/com/llenroctech/customerconnect/CustomerconnectApiApplicationTests.java
+++ backend/customerconnect-api/src/test/java/com/llenroctech/customerconnect/CustomerconnectApiApplicationTests.java
@@ -1,10 +1,15 @@
 package com.llenroctech.customerconnect;
 
+import com.llenroctech.customerconnect.service.SmsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CustomerconnectApiApplicationTests {
+
+	@MockitoBean
+	private SmsService smsService;
 
 	@Test
 	void contextLoads() {
